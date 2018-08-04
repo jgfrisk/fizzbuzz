55d26e46a733514b6109952c85712db0acb6fc97
Johan Frisk <johan.frisk@gmail.com>
2018-08-04 12:22:37 +0200

    replace % with mod

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-int main(int n)
+int mod(int a,int b)
+{
+	if ( a < b )
+		return a;
+	else
+		return mod(a-b,b);
+}
+
+int main(int n, char *s[])
 {
 	if ( n > 100 )
 		return 0;
 
-	if ( n % 15 == 0 )
+	if ( mod(n,15) == 0 )
 	{
 		puts("FizzBuzz");
 	}
-	else if ( n % 3 == 0 )
+	else if ( mod(n,3) == 0 )
 	{
 		puts("Fizz");
 	}
-	else if ( n % 5 == 0 )
+	else if ( mod(n,5) == 0 )
 	{
 		puts("Buzz");
 	}
@@ -22,7 +30,7 @@ int main(int n)
 		printf("%d\n",n);
 	}
 
-	main(n+1);
+	main(n+1,NULL);
 
 
 }
